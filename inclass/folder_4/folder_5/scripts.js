04e5318c032f7e6d41fb8ff2b3dfda9238d1f50e
Akif-Aydin <akifaydinde@gmail.com>
2024-02-12 23:32:47 +0100

    12.02.2024

---
+++ inclass/folder_4/folder_5/scripts.js
@@ -0,0 +1,7 @@
+var hasan = document.getElementById("div_2")
+var sedat = document.getElementById("div_1")
+var button = document.getElementById("button")
+
+button.onclick = function(){
+    hasan.style.backgroundColor
+}
